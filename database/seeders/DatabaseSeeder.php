f637845629378eabf9b0d7b48468f7bb662c0206
AdityaWijayanto19 <adityaputraw49@student.ub.ac.id>
2026-01-04 23:33:45 +0700

    Update DatabaseSeeder to include RoleAndPermissionSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RoleAndPermissionSeeder::class,
+            ShiftSeeder::class,
             UserSeeder::class,
             NotificationSoundSeeder::class,
             ManageContentSeeder::class,
